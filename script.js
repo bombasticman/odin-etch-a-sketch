45d6a01de41e539cac6c9d026dee25f5d3b5025f
Atakan Yalçın <atakanyaln@gmail.com>
2026-09-21 18:06:40 +0300

    created the table for the sketch

---
+++ script.js
@@ -0,0 +1,13 @@
+function populate_the_screen() {
+    for (let column = 0; column < 16; column++) {
+        for (let row = 0; row < 16; row++) {
+            const square = document.createElement("div")
+            square.classList.add("square")
+            document.getElementById("container").appendChild(square)
+        }
+    }
+}
+
+
+
+populate_the_screen()
